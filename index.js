8aa48345a40fdf1305a12ab17044f90e9c470c6e
zhaozhisheng <zhaozhisheng@aixuedai.com>
2018-11-29 17:29:30 +0800

    Update 目录更新 添加依赖包内容

--- index.js
+++ index.js
@@ -1,6 +1,114 @@
+import React, { Component } from 'react';
+import { StyleSheet, View, AppRegistry } from 'react-native';
+import StaticContainer from 'static-container';
 
-import { NativeModules } from 'react-native';
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+});
 
-const { RNReactNativeDebugtools } = NativeModules;
+class Provider extends Component {
+  getChildContext() {
+    return { store: this.props.store };
+  }
 
-export default RNReactNativeDebugtools;
+  render() {
+    return this.props.children;
+  }
+}
+
+AppRegistry.setWrapperComponentProvider(function() {
+  return function RootSiblingsWrapper(props) {
+    return (
+      <View style={styles.container}>
+        {props.children}
+        <RootSiblings />
+      </View>
+    );
+  };
+});
+
+let uuid = 0;
+const triggers = [];
+class RootSiblings extends Component {
+  _updatedSiblings = {};
+  _siblings = {};
+  _stores = {};
+
+  constructor(props) {
+    super(props);
+    this._siblings = {};
+    triggers.push(this._update);
+  }
+
+  componentWillUnmount() {
+    triggers.splice(triggers.indexOf(this._update), 1);
+  }
+
+  _update = (id, element, callback, store) => {
+    const siblings = { ...this._siblings };
+    const stores = { ...this._stores };
+    if (siblings[id] && !element) {
+      delete siblings[id];
+      delete stores[id];
+    } else if (element) {
+      siblings[id] = element;
+      stores[id] = store;
+    }
+    this._updatedSiblings[id] = true;
+    this._siblings = siblings;
+    this._stores = stores;
+    this.forceUpdate(callback);
+  };
+
+  render() {
+    const siblings = this._siblings;
+    const stores = this._stores;
+    const elements = [];
+    Object.keys(siblings).forEach(key => {
+      const element = siblings[key];
+      if (element) {
+        const sibling = (
+          <StaticContainer key={`root-sibling-${key}`} shouldUpdate={!!this._updatedSiblings[key]}>
+            {element}
+          </StaticContainer>
+        );
+
+        const store = stores[key];
+        if (store) {
+          elements.push(
+            <Provider store={store} key={`root-sibling-${key}-provider`}>
+              {sibling}
+            </Provider>
+          );
+        } else {
+          elements.push(sibling);
+        }
+      }
+    });
+    this._updatedSiblings = {};
+    return elements;
+  }
+}
+
+export default class RootSiblingManager {
+  constructor(element, callback, store) {
+    const id = uuid++;
+    function update(element, callback, store) {
+      triggers.forEach(function(trigger) {
+        trigger(id, element, callback, store);
+      });
+    }
+
+    function destroy(callback) {
+      triggers.forEach(function(trigger) {
+        trigger(id, null, callback);
+      });
+    }
+
+    update(element, callback, store);
+    this.update = update;
+    this.destroy = destroy;
+  }
+}
